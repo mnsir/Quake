aea6ea8abe3ab91326c2d4320cfae0b4b6905552
mnsir <mnsir@ya.ru>
2024-07-21 16:37:59 +0300

    _WIN32 ifdef again

--- QuakeHW/quakedef.h
+++ QuakeHW/quakedef.h
@@ -45,21 +45,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include <stdlib.h>
 #include <setjmp.h>
 
-#if defined(_WIN32) && !defined(WINDED)
-
 #if defined(_M_IX86)
 #define __i386__ 1
 #endif
 
 void VID_LockBuffer();
 void VID_UnlockBuffer();
-
-#else
-
-#define VID_LockBuffer()
-#define VID_UnlockBuffer()
-
-#endif
 
 #if defined __i386__ // && !defined __sun__
 #define id386 0
